9a9ecfcc3756831f0214af760e0aa8c30adf1d00
unknown <your email>
2022-03-30 14:28:52 -0400

    Both Endpoints functional in Postman

---
+++ ASP_NET_Video_Games_API/Controllers/GamesController.cs
@@ -0,0 +1,47 @@
+﻿using ASP_NET_Video_Games_API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_NET_Video_Games_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GamesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GamesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult GetGames()
+        {
+            var videoGamePublishers = _context.VideoGames.Select(vg => vg.Name).Distinct();
+
+            return Ok(videoGamePublishers);
+        }
+
+        //[HttpGet("{pubName}")]
+        //public IActionResult GetGamesByPublisher(string pubName)
+
+        //{
+        //    int? MaxYear = _context.VideoGames.Select(vg => vg.Year).Max();
+        //    int? MinYear = _context.VideoGames.Select(vg => vg.Year).Min();
+
+        //    var videoGames = _context.VideoGames.Where(vg => vg.Publisher == pubName);
+
+        //    return Ok(videoGames);
+        //}
+        [HttpGet("{Id}")]
+        public IActionResult GetGamesById(int Id)
+        {
+            var videoGames = _context.VideoGames.Where(vg => vg.Id == Id);
+
+            return Ok(videoGames);
+        }
+    }
+}
+            
+    
+
